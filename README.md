e8b454def93c35df27732fb15d510a5cbd284de4
JoshuaShepard87 <Joshua.burden@beeso.io>
2022-10-10 23:24:42 -0500

    commit!

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # react
+
+## First App 
+
+created using typescript
+
+## Second App 
+built with the create-react-app command and runs
---
+++ first-app/src/challenge.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+const test = (
+    <ul>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+    </ul>
+)
+
+ReactDOM.render(test, document.getElementById('app-root'))
